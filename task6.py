6afd38be91d1da119f2b0f4da26462ea17509038
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-13 12:44:51 +0200

    added task 6

---
+++ task6.py
@@ -0,0 +1,9 @@
+# White a function that takes in three numbers and returns the maximum number. Do this without using any builtin methods. Write your own logic from scratch.
+
+def max_number(x, y, z):
+    if z >= x and z >= y:
+        return(z)
+    if y >= x and y >= z:
+        return(y)
+    if x >= y and x >= z:
+        return(x)
