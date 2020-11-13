963c654d0ff24a4aab947979350ee1c1d35973f3
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-13 12:08:31 +0200

    added task5

---
+++ task5.py
@@ -0,0 +1,13 @@
+# Write a function that takes in three numbers. These numbers represent the lengths of the sides of a triangle. The function should return the area of a triangle.
+
+from math import sqrt
+
+def triangle(x, y, z):
+    s = 0.5 * (x + y + z)
+    s1 = s - x
+    s2 = s - y
+    s3 = s - z
+    s4 = s1 * s2 * s3
+    pre_area = s * s4
+    area = sqrt(pre_area)
+    return (area)
