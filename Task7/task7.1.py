4c277713b2fe87d77b00ac1bd7450f50d6621f41
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-13 13:12:18 +0200

    added task 7

---
+++ Task7/task7.1.py
@@ -0,0 +1,8 @@
+# Write a function that takes in a number representing the temperature in Celsius and returns the temperature in Fahrenheit. Write another function that does the opposite (Fereignheit to Celsius)
+
+def fahrenheit(x):
+    f = x * (9/5)
+    f = f + 32
+    return(f)
+
+print(fahrenheit(100))
---
+++ Task7/task7.2.py
@@ -0,0 +1,4 @@
+def celsius(x):
+    c = x - 32
+    c = c * (5/9)
+    return(c)
