fcf34642c9785555efe11e3befbc9f9f7f51bdc5
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-13 11:06:55 +0200

    added task 3

---
+++ task3.py
@@ -0,0 +1,11 @@
+#Write a function that takes 2 numbers as input. 
+#If either of the numbers is 65, OR if the sum of the numbers is 65 then return true. Otherwise return false
+
+def my_function(x, y):
+    sum = x + y
+    if x == 65 or y == 65 or sum == 65:
+        return (True)
+    else:
+        return (False)
+
+
