dbfe067e8d9fce60802b9e8e1c624fd5c8b800ec
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-13 11:29:30 +0200

    added task 4

---
+++ task4.py
@@ -0,0 +1,16 @@
+#  Write a function that takes 2 numbers as input. 
+# If either of the numbers is 3 AND the sum of the numbers contains a 3 then return true. Otherwise return false
+
+def abstract(x, y):
+    sum = x + y
+    sum = str(sum)
+    i = 0
+    while i < len(sum):
+        if sum[i] == '3':
+            return (True)
+        i += 1
+    if x == 3 or y == 3:
+        return (True)
+    else:
+        return (False)
+    
