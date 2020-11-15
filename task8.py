977d0036da089bbc93ea821d1eca05bf0caf1881
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-16 01:02:59 +0200

    fixed errors

---
+++ task8.py
@@ -0,0 +1,18 @@
+def convert(time):
+    time = int(time)
+    hour = int(time / 60)
+    minutes = time % 60 
+    if time < 60 and minutes == 1:
+        print(minutes, "minute")
+    elif time < 60:
+        print(minutes, "minutes")
+    elif hour == 1 and minutes == 1:
+        print(hour, "hour,", minutes, "minute")
+    elif hour > 1 and minutes == 1:
+        print(hour, "hours,", minutes, "minute")
+    elif hour == 1:
+        print(hour, "hour,", minutes, "minutes")
+    else:
+        print(hour, "hours,", minutes, "minutes")
+
+convert(121)
