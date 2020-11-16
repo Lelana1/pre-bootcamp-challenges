0af2124a7dd0e42f3661d1aea367e9f6b6f62b2e
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-16 16:17:54 +0200

    added task11.py

---
+++ task11.py
@@ -0,0 +1,15 @@
+def common_letters(str1, str2):
+    common = []
+    s = ", "
+
+    for i in str1:
+        for j in str2:
+            if j == i:
+                if i in common:
+                    continue
+                else:
+                    common.append(i)
+    print(s.join(common))
+
+
+common_letters("house", "computers")
