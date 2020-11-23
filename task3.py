4ddb5d41f8b537af53a01aad85f99413db80005c
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-23 15:12:40 +0200

    Applied changes per feedback

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
 #Write a function that takes 2 numbers as input. 
 #If either of the numbers is 65, OR if the sum of the numbers is 65 then return true. Otherwise return false
 
-def sixtyFive_checker(x, y):
+def sixty_Five_checker(x, y):
     sum = x + y
     if x == 65 or y == 65 or sum == 65:
         return True
