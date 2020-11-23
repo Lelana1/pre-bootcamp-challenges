92a2d5940a13720fe068d7ce647a1890e0ac5714
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-23 15:56:50 +0200

    Applied changes per feedback

--- task4.py
+++ task4.py
@@ -1,9 +1,9 @@
 #  Write a function that takes 2 numbers as input. 
 # If either of the numbers is 3 AND the sum of the numbers contains a 3 then return true. Otherwise return false
 
-def three_checker(x, y):
+def three_checker(x,y):
     sum = x + y
-    if x == 3 or y == 3 and '3' in sum:
+    if (x or y) == 3 and '3' in str(sum):
         return True
     else:
         return False
