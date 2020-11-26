01da10cb121f3844c367c84af5e7697af94a3126
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2020-11-26 02:47:20 +0200

    Modified task 4 per feedback
    I understand the logic better now and that the function has to return true only when both conditions are met.

--- task4.py
+++ task4.py
@@ -3,7 +3,7 @@
 
 def three_checker(x,y):
     sum = x + y
-    if (x or y) == 3 and '3' in str(sum):
+    if x == 3 or y == 3 and '3' in str(sum):
         return True
     else:
         return False
